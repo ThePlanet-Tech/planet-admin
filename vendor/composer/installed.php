8d95d892c83fd792301a8d1ce0fab53d4348bfcd
Radwan <muhammed.a.radwan@gmail.com>
2022-06-26 17:00:49 +0200

    add composer autoload psr

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '386990dd6d0952e73e66aacb7f13a0cbd03c4cba',
+    'name' => 'theplanet-tech/planet-admin',
+  ),
+  'versions' => 
+  array (
+    'theplanet-tech/planet-admin' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '386990dd6d0952e73e66aacb7f13a0cbd03c4cba',
+    ),
+  ),
+);
